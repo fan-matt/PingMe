074afa6a3b990c9fbed1d11aff3ec0e1fb13a3f3
Matthew Fan <fanma@ucla.edu>
2019-08-07 17:26:04 -0500

    Removed unnecessary files and started page creation

---
+++ src/components/Page.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import React from 'react';
+
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Page;
---
+++ src/components/QuickRemindersPage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import React from 'react';
+import Page from './Page.js';
+
+
+class QuickRemindersPage extends React.Component {
+    render() {
+        return (
+            <Page>
+                
+            </Page>
+        );
+    }
+}
+
+export default QuickRemindersPage;
